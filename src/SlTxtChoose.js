616006e2fbd7304ddb85b02e92c73b405b388576
Irene <irene.bhuiyan@gmail.com>
2019-06-10 17:30:37 +0100

    updated choose pages

--- src/SlTxtChoose.js
+++ src/SlTxtChoose.js
@@ -8,19 +8,25 @@ function SlTxtChoose() {
         <div className="back"><Link to="/"><span className="fa fa-chevron-left"></span></Link></div><p className="text-center">I Hear You</p>
       </div>
       <div className="sl-txt-choose">
-        <h3 className="text-center">Select Your Language</h3>
-        <br/><br/>
-        <p><img className="icons" src="img/wave.svg" alt="icon of hand waving"/> From:</p>
-        <select className="form-control">
-          <option>British Sign Language</option>
-        </select>
+        <div className="illustration-container">
+          <img className="illustration" src="img/illustration-talk.svg" alt="illustration of someone being live-translated"/>
+        </div>
         <br/>
-        <p><img className="icons" src="img/text.svg" alt="icon of speech bubble"/> To:</p>
-        <select className="form-control">
-          <option>English</option>
-        </select>
-        <br/><br/>
-        <Link to="sl-txt" className="d-flex justify-content-center"><button type="button" className="btn btn-success">Go!</button></Link>
+        <div className="choose-container">
+          <h3 className="text-center">Select Your Language</h3>
+          <br/>
+          <p><img className="icons" src="img/wave.svg" alt="icon of hand waving"/> From:</p>
+          <select>
+            <option>British Sign Language</option>
+          </select>
+          <br/><br/>
+          <p><img className="icons" src="img/text.svg" alt="icon of speech bubble"/> To:</p>
+          <select>
+            <option>English</option>
+          </select>
+          <br/><br/>
+          <Link to="sl-txt" className="d-flex justify-content-center"><button type="button" className="btn btn-success">Go</button></Link>
+        </div>
       </div>
     </div>
   );
